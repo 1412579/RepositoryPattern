9de0b10e767984c1c3f4446dafb5d2abc15b2180
Tri Vu <content.itvmtri@gmail.com>
2019-05-10 12:59:40 +0700

    update readme + db

---
+++ README.md
@@ -0,0 +1,23 @@
+# Demo RP
+
+## Thông tin
+
+    - Chạy ASP.NET CORE 2.1
+	
+	- Sử dụng Microsoft.EntityFrameworkCore để connect với DB
+
+### Cấu hình
+
+    - B1: Vào SQLSERVER tạo một DB với tên RP
+
+    - B2: Chạy Script DB trong Folder SQL
+
+    - B3: Mở Source -> Models -> Chọn RPContext -> Chỉnh lại connectionString cho đúng theo máy
+	
+	![alt text](https://imgur.com/BHkbwF5)
+
+	- B4: Mở source và chạy thử để test
+
+### Hình ảnh DEMO 
+
+	![alt text](https://imgur.com/wEuNurc)
---
+++ SQL/RP.sql
@@ -0,0 +1,18 @@
+USE [RP]
+GO
+/****** Object:  Table [dbo].[ProductCategory]    Script Date: 4/25/2019 9:38:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[ProductCategory](
+	[CategoryID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](255) NULL,
+ CONSTRAINT [PK_ProductCategory] PRIMARY KEY CLUSTERED 
+(
+	[CategoryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
